3a160be23bcdc7adf99847cf5be88545ef568c0b
Floris Janssen <info@janssendigital.com>
2026-08-23 18:49:40 +0200

    Initial build: bijgebouwvergunning.nl based on the JDD site template

---
+++ src/lib/serviceImages.ts
@@ -0,0 +1,6 @@
+// The default photo used behind every Services card on the homepage.
+// Swap this per site to something that matches that site's niche.
+export const DEFAULT_SERVICE_IMAGE = {
+  src: "/images/tuinhuis-schuur-hero.jpg",
+  alt: "Houten tuinhuis met schuine kap tussen het groen in een tuin",
+};
